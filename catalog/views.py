39cea07aee11cadd20154e9b712bdcaa48b6abc8
OMEN - Windows 11 <david-g-v-1997@hotmail.com>
2025-10-02 23:13:28 +0200

    Chagenge Part 5

--- catalog/views.py
+++ catalog/views.py
@@ -16,11 +16,20 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    #Challenge
+    num_genres = Genre.objects.count()
+    q = request.GET.get("q", "").strip()
+    num_book_match = 0
+    num_book_match = Book.objects.all().filter(title__icontains=q).count() if q else 0
+
     context = {
         'num_books':num_books,
         'num_instances':num_instances,
         'num_instances_available':num_instances_available,
         'num_authors':num_authors,
+        'num_genres':num_genres,
+        'num_book_match':num_book_match,
+        'q':q,
     }
 
     # Render the HTML template index.html with the data in the context variable
